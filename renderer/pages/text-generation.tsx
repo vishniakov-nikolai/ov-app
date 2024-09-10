6bb97b8b945adfdcfff7713d67ea334eadc24858
Vishniakov <nikolai.vishniakov@intel.com>
2024-09-10 14:00:53 +0200

    Decorate alert

--- renderer/pages/text-generation.tsx
+++ renderer/pages/text-generation.tsx
@@ -10,13 +10,13 @@ import { BE, UI } from '../../constants';
 import { Label } from '../components/ui/label';
 import { Input } from '../components/ui/input';
 import { Header } from '../components/header';
+import { AlertDialog, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from '../components/ui/alert-dialog';
 
 const DEFAULT_DEVICE = 'AUTO';
 
-export default function ImageClassificationPage() {
+export default function TextGenerationPage() {
   const searchParams = useSearchParams()
   const modelName = searchParams.get('model');
-  const [isModelDownloading, setIsModelDownloading] = useState(false);
   const [input, setInput] = useState('');
   const [isInferenceRunning, setIsInferenceRunning] = useState(false);
   const [selectedDevice, setSelectedDevice] = useState(DEFAULT_DEVICE);
@@ -28,6 +28,8 @@ export default function ImageClassificationPage() {
   const [noRepeatNgramSize, setNoRepeatNgramSize] = useState('0');
   const [numBeams, setNumBeams] = useState('1');
   const [numReturnSequences, setNumReturnSequences] = useState('1');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [showError, setShowError] = useState(false);
 
   useEffect(() => {
     if (!modelName || !selectedDevice) return;
@@ -62,9 +64,10 @@ export default function ImageClassificationPage() {
     );
   }, []);
   useEffect(() => {
-    return window.ipc.on(UI.EXCEPTION, (errorMessage) => {
+    return window.ipc.on(UI.EXCEPTION, (errorMessage: string) => {
       setIsInferenceRunning(false);
-      alert(errorMessage);
+      setErrorMessage(errorMessage);
+      setShowError(true);
     });
   }, []);
   useEffect(() => {
@@ -225,6 +228,23 @@ export default function ImageClassificationPage() {
             </> }
         </div>
         <Footer className="mt-auto" />
+        <AlertDialog open={showError} onOpenChange={setShowError}>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle className="flex items-center">
+                <div className="text-destructive w-8 h-8 mr-2">
+                  <svg className="w-full h-full" stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" xmlns="http://www.w3.org/2000/svg"><polygon points="7.86 2 16.14 2 22 7.86 22 16.14 16.14 22 7.86 22 2 16.14 2 7.86 7.86 2"></polygon><line x1="12" x2="12" y1="8" y2="12"></line><line x1="12" x2="12.01" y1="16" y2="16"></line></svg>
+                </div>
+                <span>Something went wrong</span>
+              </AlertDialogTitle>
+              <AlertDialogDescription>{errorMessage}</AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Ok</AlertDialogCancel>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+
       </div>
 
     </React.Fragment>
